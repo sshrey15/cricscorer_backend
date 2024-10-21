eda480e10f5a29d725c598894ac7dda4e9b6e83b
sshrey15 <stud22.sks2@gec.ac.in>
2024-10-21 08:37:10 +0530

    scorecard

--- src/controllers/Match.js
+++ src/controllers/Match.js
@@ -33,7 +33,7 @@ export async function createMatch(req, res,next) {
     if (!team2 || team2.roompassword !== team2Password) {
       return res.status(400).json({ error: "Invalid team2 credentials" });
     }
-    
+
     const match = await prisma.match.create({
       data: {
         team1Id: team1.id,
@@ -61,11 +61,29 @@ export async function createMatch(req, res,next) {
   }
 }
 
-export async function getAllMatches(req,res, next){
-  try{
-    const matches = await prisma.match.findMany();
-    return res.status(200).json({matches});
-  }catch(err){
-    return res.status(500).json({error: "Something went wrong", err});
+
+export async function getAllMatches(req, res, next) {
+  try {
+    const matches = await prisma.match.findMany({
+      select: {
+        id: true,
+        team1Id: true,
+        team2Id: true,
+        overs: true,
+        createdAt: true,
+        innings: {
+          select: {
+            id: true,
+            inningsNumber: true,
+            inningsType: true,
+            totalRuns: true,
+            createdAt: true
+          }
+        }
+      }
+    });
+    return res.status(200).json({ matches });
+  } catch (err) {
+    return res.status(500).json({ error: "Something went wrong", err });
   }
 }
